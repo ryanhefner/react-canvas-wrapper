ef20f85a3af6b4e4ae114e7a33d1b12c2860bb64
Ryan Hefner <hi@ryanhefner.com>
2020-08-06 05:18:33 -0400

    Refactor Canvas to functional component that utilizes hooks and forwardRef

--- src/index.js
+++ src/index.js
@@ -1,88 +1,52 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { forwardRef, useEffect, useMemo, useState, useRef } from 'react';
 import cleanProps from 'clean-react-props';
 import nanoid from 'nanoid';
 
-class Canvas extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      instanceKey: nanoid(),
-    };
-  }
-
-  componentDidMount() {
-    this.draw(this.props);
-  }
-
-  componentDidUpdate() {
-    this.draw(this.props);
-  }
-
-  draw(props) {
-    if (!this.canvas) {
-      return;
-    }
-
-    props.draw(this.canvas);
-  }
-
-  render() {
-    const {
-      canvasRef,
-      width,
-      height,
-      pixelRatioAware,
-      style,
-    } = this.props;
-
-    const {
-      instanceKey,
-    } = this.state;
-
-    const pixelRatio = pixelRatioAware
-      ? window.devicePixelRatio || 1
-      : 1;
-
-    const ref = canvasRef
-      ? canvasRef
-      : (element) => { this.canvas = element; };
-
-    const styleProp = Object.assign({}, style, {
-      width,
-      height,
-    });
-
-    return (
-      <canvas
-        {...cleanProps(this.props, ['width', 'height'])}
-        key={instanceKey}
-        ref={ref}
-        width={width * pixelRatio}
-        height={height * pixelRatio}
-        style={styleProp}
-      />
-    );
-  }
-}
-
-Canvas.propTypes = {
-  canvasRef: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.shape({ current: PropTypes.elementType }),
-  ]),
-  draw: PropTypes.func,
-  width: PropTypes.number,
-  height: PropTypes.number,
-  pixelRatioAware: PropTypes.bool,
-};
-
-Canvas.defaultProps = {
+const defaultProps = {
   draw: () => {},
   width: 30,
   height: 30,
   pixelRatioAware: true,
+  style: {}
 };
+
+const Canvas = forwardRef((props, ref) => {
+  const {
+    draw,
+    width,
+    height,
+    pixelRatioAware = true,
+    style,
+  } = useMemo(
+    () => Object.assign({}, defaultProps, props),
+    [JSON.stringify(props)]
+  );
+
+  const [instanceKey] = useState(nanoid());
+  const internalRef = ref || useRef();
+
+  useEffect(() => {
+    draw(internalRef);
+  }, [JSON.stringify(props)]);
+
+  const styleProp = useMemo(() => {
+    return Object.assign({}, style, { width, height });
+  }, [JSON.stringify(style), width, height]);
+
+  const pixelRatio = pixelRatioAware
+    ? window.devicePixelRatio || 1
+    : 1;
+
+  return (
+    <canvas
+      {...cleanProps(props)}
+      key={instanceKey}
+      ref={internalRef}
+      width={width * pixelRatio}
+      height={height * pixelRatio}
+      style={styleProp}
+    />
+  );
+});
 
 export default Canvas;
